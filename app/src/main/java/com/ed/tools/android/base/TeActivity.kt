358089b242e1678b49265609f4a0b3420c40e86b
July <2023>
2023-09-22 15:22:22 +0800

    1.更改方法名

--- app/src/main/java/com/ed/tools/android/base/TeActivity.kt
+++ app/src/main/java/com/ed/tools/android/base/TeActivity.kt
@@ -1,12 +1,16 @@
 package com.ed.tools.android.base
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.app.tools.EasyWebView
 
 class TeActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tv)
-
+        val web=findViewById<EasyWebView>(R.id.web)
+        web.goAntoPath("",this)
     }
 }
